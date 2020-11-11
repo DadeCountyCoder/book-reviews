d298a0cc380df6dba336156d1963e30e6f63a68a
DadeCountyCoder <dadecountycoder@gmail.com>
2020-11-11 12:26:50 -0500

    user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,37 +1,56 @@
 class UsersController < ApplicationController
 
-  # GET: /users
-  get "/users" do
-    erb :"/users/index.html"
-  end
-
+  
   # GET: /users/new
-  get "/users/new" do
+  get "/signup" do
     erb :"/users/new.html"
   end
 
-  # POST: /users
-  post "/users" do
-    redirect "/users"
+  get "/login" do
+    erb :"/users/login.html"
   end
 
-  # GET: /users/5
+  post "/login" do
+    #session[:user_id] = user.id
+    redirect '/users/:id'
+  end
+
+  post "/logout" do
+    # session.destroy
+    redirect '/'
+  end
+
+  # shows all reviews by user
   get "/users/:id" do
     erb :"/users/show.html"
   end
 
-  # GET: /users/5/edit
-  get "/users/:id/edit" do
-    erb :"/users/edit.html"
+# create new user
+  post "/users/new" do
+    #create new user from paprams
+    #send validation
+    redirect "/login"
   end
+
+
+
+# GET: /users
+  # get "/users" do
+  #   erb :"/users/index.html"
+  # end
+
+  # GET: /users/5/edit
+  # get "/users/:id/edit" do
+  #   erb :"/users/edit.html"
+  # end
 
   # PATCH: /users/5
-  patch "/users/:id" do
-    redirect "/users/:id"
-  end
+  # patch "/users/:id" do
+  #   redirect "/users/:id"
+  # end
 
   # DELETE: /users/5/delete
-  delete "/users/:id/delete" do
-    redirect "/users"
-  end
+  # delete "/users/:id/delete" do
+  #   redirect "/users"
+  # end
 end
